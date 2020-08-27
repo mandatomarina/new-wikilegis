212e9022c532b81ef18a0b7d9b67707f2e9ee304
Pedro Markun <pedro@markun.com.br>
2020-08-27 19:24:17 -0300

    emenda especifica

--- src/apps/mutirao/urls.py
+++ src/apps/mutirao/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MutiraoView
+from .views import MutiraoView, MutiraoRnd
 
 urlpatterns = [
-    path('<str:tipo>/<int:numero>/<int:ano>', MutiraoView.as_view(), name="mutirao"),
+    path('<str:tipo>/<int:numero>/<int:ano>', MutiraoRnd.as_view(), name="mutirao_rnd"),
+    path('<str:tipo>/<int:numero>/<int:ano>/emenda/<int:numero_emenda>/<int:ano_emenda>', MutiraoView.as_view(), name="mutirao")
 ]
--- src/apps/mutirao/views.py
+++ src/apps/mutirao/views.py
@@ -4,6 +4,14 @@ from .models import Emenda, Review
 from .forms import MutiraoForm
 from django.db.models import Count
 
+
+class MutiraoRnd(View):
+    def get(self, request, *args, **kwargs):
+        emenda = Emenda.objects.filter(pl__tipo=self.kwargs['tipo'],pl__numero=self.kwargs['numero'],pl__ano=self.kwargs['ano']).annotate(r_count=Count('review'))
+        e= emenda.filter(r_count__lte=2).order_by('?').first()
+        return redirect('mutirao', tipo=self.kwargs['tipo'], ano=self.kwargs['ano'], numero=self.kwargs['numero'], ano_emenda=e.ano, numero_emenda=e.numero)
+
+
 class MutiraoView(View):
     def post(self, request, *args, **kwargs):
 
@@ -14,20 +22,20 @@ class MutiraoView(View):
             obj.owner = request.user
             obj.emenda = Emenda.objects.get(pk=form.cleaned_data['emenda_id'])
             obj.save()
-            return redirect('mutirao', tipo=self.kwargs['tipo'], ano=self.kwargs['ano'], numero=self.kwargs['numero'])
+            return redirect('mutirao_rnd', tipo=self.kwargs['tipo'], ano=self.kwargs['ano'], numero=self.kwargs['numero'])
 
     
     def get(self, request, *args, **kwargs):
         emenda = Emenda.objects.filter(pl__tipo=self.kwargs['tipo'],pl__numero=self.kwargs['numero'],pl__ano=self.kwargs['ano']).annotate(r_count=Count('review'))
         
-        emenda_revisar = emenda.filter(r_count__lt=2).order_by('?')
+        emenda_revisar = emenda.filter(numero=self.kwargs['numero_emenda'],ano=self.kwargs['ano_emenda']).first()
         emenda_revisada = emenda.filter(r_count__gte=1)
         if not emenda_revisar:
             return render(request, 'mutirao/obrigado.html', { 'tipo' : self.kwargs['tipo'], 'numero' : self.kwargs['numero'], 'ano' : self.kwargs['ano']})
         
-        form = MutiraoForm({'emenda_id' : emenda[0].id, 'apoiar':False})
+        form = MutiraoForm({'emenda_id' : emenda_revisar.id, 'apoiar':False})
         context = {
-            'emenda' : emenda_revisar[0],
+            'emenda' : emenda_revisar,
             'form' : form,
             'emendas_total' : len(emenda),
             'emendas_analisadas' : len(emenda_revisada)
